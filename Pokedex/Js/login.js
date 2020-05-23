612837a4cb9f353268af55b8a1af622faf18f7e7
EmmanuelSalinas <salinas.e9810@gmail.com>
2020-05-23 17:47:28 -0500

    se agrego IU de pokedex

--- Pokedex/Js/login.js
+++ Pokedex/Js/login.js
@@ -1,19 +1,22 @@
 window.onload = init;
 
 function init (){
-    document.querySelector('.btn-secondary').addEventListener('click', function(){
-        window.location.href = "signin.html";
-    });
+    if(!localStorage.getItem("token")){
 
-    document.querySelector('.btn-primary').addEventListener('click' , login);
+        document.querySelector('.btn-secondary').addEventListener('click', function(){
+            window.location.href = "signin.html";
+        });
     
+        document.querySelector('.btn-primary').addEventListener('click' , login);
+    }
+    else{
+        window.location.href = "pokedex.html";
+    }
 }
 
 function login(){
     var mail = document.getElementById('input-mail').value;
     var pass = document.getElementById('input-password').value;
-
-    console.log(mail,pass);
 
     axios({
         method:  'post',
@@ -23,10 +26,17 @@ function login(){
             user_password: pass
         }
     }).then(function(res){
-        console.log(res);
+        //console.log(res.data);
+        if(res.data.code == 200){
+            //alert("Inicio Exitoso")
+            localStorage.setItem("token" , res.data.message);
+            window.location.href="pokedex.html";
+        }
+        else{
+            alert("Usuario y/o Contraseña Incorrectos")
+        }
     }).catch(function(err){
-        console.log(err);
-        
+        console.log(err); 
     });
     
 
